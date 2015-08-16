3cfc2a4de4ee546f7f805a288a5646bcf11ff6ce
luklewluk <luklewluk@gmail.com>
2015-08-16 18:29:42 +0200

    Routing skeleton

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,10 +11,19 @@
 |
 */
 
-/*
-Route::get('/', function () {
-    return view('welcome');
-});
-*/
-
 Route::get('/', 'HomeController@index');
+
+// TODO: Create methods below
+Route::get('/user/login', 'UserController@login');
+Route::get('/user/register', 'UserController@create');
+// Library management & list of your games
+Route::get('/user/library', 'UserController@index');
+// Pass/email/nick change...
+Route::get('/user/profile', 'UserController@edit');
+Route::get('/profile/{id}', 'UserController@show');
+// Compare user's library with yours
+Route::get('/profile/{id}/compare', 'UserController@compare');
+// Search by id/name/email
+Route::get('/profile/search', 'UserController@search');
+// Game details
+Route::get('/game/{id}', 'GameController@show');
