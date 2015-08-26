1157c28297a77b6399a9f9653ff8ca7d6c6566d7
luklewluk <luklewluk@gmail.com>
2015-08-26 19:51:03 +0200

    Games table modified

---
+++ database/migrations/2015_08_26_172652_modify_games_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ModifyGamesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('games', function (Blueprint $table) {
+            $table->dateTime('original_release_date')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
